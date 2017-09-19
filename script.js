1d31a5b668b2568f00fc9cb381c1c996eb5ab6de
JaredGuttromson <jaredguttromson@hotmail.com>
2017-09-19 17:53:09 -0600

    Fixed Javascript

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 window.onload = function() {
 
-	var htmlSection = document.getElementsById("htmlSection");
-	//var cssSection = ("cssSection");
-	//var jsSection = findElementById("javaScriptSection");
+	var htmlSection = document.getElementById("htmlSection");
+	var cssSection = document.getElementById("cssSection");
+	var jsSection = document.getElementById("javascriptSection")
 
 	htmlSection.onclick = function(e) {
-		var dropdown = document.getElementsByClassName("content");
-		dropdown.classList.toggle("show");
+		var dropdown = htmlSection.getElementsByClassName("htmlContent");
+
+		for (var i = 0; i < dropdown.length; i++) {
+			dropdown[i].classList.toggle("show");
+		}
+	}
+
+	cssSection.onclick = function(e) {
+		var dropdown = cssSection.getElementsByClassName("cssContent");
+
+		for (var i = 0; i < dropdown.length; i++) {
+			dropdown[i].classList.toggle("show");
+		}
+	}
+
+	jsSection.onclick = function(e) {
+		var dropdown = javascriptSection.getElementsByClassName("javascriptContent");
+
+		for (var i = 0; i < dropdown.length; i++) {
+			dropdown[i].classList.toggle("show");
+		}
 	}
 }
